073de6c36a6855665c98aa11b26556afcb6646d0
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-12-30 22:33:24 +0100

    Adding user Information to file

--- SystemSecurity.cpp
+++ SystemSecurity.cpp
@@ -35,7 +35,30 @@ int main() {
   			cin >> originalPassword;
   			cout << "Please enter your age: ";
   			cin >> age;
+  			
+  			ofstream of1;
+  			of1.open("file.txt");
+  			if(of1.is_open())
+  			{
+  				of1 << userName << "\n ";
+				of1 << originalPassword	;
+				cout << "Your registration was successful" << endl;
+			}
   			break;
+  			
+  		case 2:
+  			
+  			i = 0;
+  			cout << "________________________" << endl;
+  			cout << "--------LOGIN--------" << endl;
+  			cout << "________________________" << endl;
+  			ifstream of2;
+  			of2.open("file.txt");
+  			cout << "Please enter username: ";
+  			cin >> userName;
+  			cout << "Please enter password: ";
+  			cin >> pass;
+  			
 
   	}
 }
